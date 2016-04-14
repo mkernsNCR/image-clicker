7eecd1417d541ddaace4251c1b6f672a835acd53
Nyron-Waite <nyroncurtiswaite@gmail.com>
2016-04-14 10:21:39 -0400

    hbs added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var hbs = require("express-handlebars");
 
 var app = express();
 
@@ -9,4 +10,4 @@ app.get("/", function(req, res){
 
 app.listen(3005, function(){
   console.log("were live");
-})
+});
