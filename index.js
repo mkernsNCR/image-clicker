60ebb8334c1be4859a4f0c3abfadf12d63d43862
Nyron-Waite <nyroncurtiswaite@gmail.com>
2016-04-14 11:17:22 -0400

    made seed data and loaded to index route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var hbs = require("express-handlebars");
+var db = require("./db/connection");
 
 var app = express();
 
@@ -10,8 +11,19 @@ app.engine(".hbs", hbs({
   layoutsDir: "views/",
   defaultLayout: "layout-main"
 }));
+
+app.use("/assets", express.static("public"));
+
+
 app.get("/", function(req, res){
     res.render("app-welcome");
+});
+
+app.get("/images", function (req, res) {
+  res.render("images-index", {
+    cool: "hello" ,
+    images: db.images
+  });
 });
 
 app.listen(3005, function(){
